e38539e9a9876797e650d5ca33bdda9c7ce3123d
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-06 00:02:44 -0300

    feat: Adiciona pasta images e arquivos HTML e JS para primeira atividade com JS (browser)

---
+++ cap06/scripts/resp6_a.js
@@ -0,0 +1,71 @@
+const form = document.querySelector("form");
+const btnListarClubes = document.querySelector(".listar-clubes");
+const btnMontarTabela = document.querySelector(".montar-tabelas");
+const resp = document.querySelector("pre");
+const paragraph = document.querySelector("p");
+
+const clubes = [];
+
+function adicionarEMostrar(e) {
+  e.preventDefault();
+
+  paragraph.innerText = "";
+
+  const clubeNome = form.inClube.value.trim();
+
+  const nomeValido = verificarNome(clubeNome);
+  if (!nomeValido) return;
+
+  clubes.push(clubeNome);
+  mostrarTimes();
+  form.reset();
+}
+
+function mostrarTimes() {
+  if (!clubes.length) {
+    paragraph.innerText = "Não possui times adicionados.";
+    return;
+  }
+
+  paragraph.innerText = "";
+  resp.innerText = "";
+
+  for (const clube of clubes) {
+    resp.innerText += `${clube} \n`;
+  }
+}
+
+function montarTabelas() {
+  if (!clubes.length || !(clubes.length % 2 === 0)) {
+    paragraph.innerText =
+      "Precisa ter pelo menos dois clubes ou pares de clubes.";
+    return;
+  }
+
+  paragraph.innerText = "";
+  resp.innerText = "";
+  const clubesTabelas = clubes.slice();
+
+  for (let i = 0; i < clubes.length / 2; i++) {
+    resp.innerText += `${clubesTabelas.shift()} x ${clubesTabelas.pop()} \n`;
+  }
+}
+
+function verificarNome(clubeNome) {
+  if (clubeNome === "") {
+    paragraph.innerText = "Digite um nome válido.";
+    form.inClube.focus();
+    return false;
+  }
+  if (clubes.includes(clubeNome)) {
+    paragraph.innerText = "Já existe um clube com esse nome.";
+    form.inClube.focus();
+    return false;
+  }
+
+  return clubeNome;
+}
+
+form.addEventListener("submit", adicionarEMostrar);
+btnListarClubes.addEventListener("click", mostrarTimes);
+btnMontarTabela.addEventListener("click", montarTabelas);
