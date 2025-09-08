b2dc5c481bdec1dcf718fcfec4033765c11ced84
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-08 10:48:35 -0300

    feat: Adiciona imagem e arquivos HTML e JS para a segunda  atividade com JS (browser)

---
+++ cap06/scripts/resp6_b.js
@@ -0,0 +1,74 @@
+const form = document.querySelector("form");
+const btn = document.querySelector("#btnVerificarOrdem");
+const exibirResultado = document.querySelector("pre");
+const exibirAviso = document.querySelector("p");
+
+const numeros = [];
+
+function limparSaida() {
+  exibirAviso.innerText = "";
+  exibirResultado.innerText = "";
+}
+
+function validarNumero(valor) {
+  if (isNaN(valor) || valor <= 0) {
+    exibirAviso.innerText = "Digite um número válido maior que zero.";
+    return false;
+  }
+  if (numeros.includes(valor)) {
+    exibirAviso.innerText = "Esse número já existe na lista.";
+    return false;
+  }
+  return valor;
+}
+
+function mostraNaTela() {
+  if (!numeros.length) {
+    exibirAviso.innerText = "Não existe números na lista.";
+  }
+
+  limparSaida();
+
+  for (const numero of numeros) {
+    exibirResultado.innerText += `${numero} \n`;
+  }
+}
+
+function adicionarNumero(e) {
+  e.preventDefault();
+
+  const numero = parseInt(form.inNumero.value, 10);
+  const validacaoDeNumero = validarNumero(numero);
+
+  if (!validacaoDeNumero) {
+    return;
+  }
+
+  limparSaida();
+
+  numeros.push(numero);
+  mostraNaTela();
+  form.inNumero.focus();
+  form.reset();
+}
+
+function verificarOrdemNumerica() {
+  limparSaida();
+
+  if (numeros.length < 2) {
+    exibirAviso.innerText =
+      "É preciso ter pelo menos 2 números para verificar a ordem.";
+    return;
+  }
+
+  for (let i = 0; i < numeros.length - 1; i++) {
+    if (numeros[i] > numeros[i + 1]) {
+      exibirAviso.innerText = "A lista de número NÃO está em ordem crescente.";
+      return;
+    }
+  }
+  exibirAviso.innerText = "A lista de número está em ordem crescente.";
+}
+
+form.addEventListener("submit", adicionarNumero);
+btn.addEventListener("click", verificarOrdemNumerica);
